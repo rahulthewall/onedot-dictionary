de88dad4e2e3ec254e1ac9182c7c1ac8a8e25547
Rahul Jain <Rahul.Jain@r3pi.io>
2019-09-05 22:14:03 +0200

    add ant

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { store } from "./modules/redux";
 import { Dictionaries, NoMatch, Dictionary } from "./routes";
 
+import 'antd/dist/antd.css';
+
 const App: React.FC = () => {
   return (
     <Provider store={store}>
