5b29d5fa1df896d12d9fd422f8d6c223151bf905
Rahul Jain <Rahul.Jain@r3pi.io>
2019-09-08 09:42:14 +0200

    Make title a link to go home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import { Layout, Typography } from 'antd';
 
 import { store } from './modules/redux';
@@ -11,20 +11,22 @@ import styles from './App.module.css';
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Layout>
-        <Layout.Header className={styles.appHeader}>
-          <Typography.Title>Dictionary Management Application</Typography.Title>
-        </Layout.Header>
-        <Layout.Content>
-          <BrowserRouter>
+      <BrowserRouter>
+        <Layout>
+          <Layout.Header className={styles.appHeader}>
+            <Typography.Title>
+              <NavLink to="/">Dictionary Management Portal</NavLink>
+            </Typography.Title>
+          </Layout.Header>
+          <Layout.Content>
             <Switch>
               <Route exact path="/" component={Dictionaries} />
               <Route exact path="/dictionary/:id" component={Dictionary} />
               <Route component={NoMatch} />
             </Switch>
-          </BrowserRouter>
-        </Layout.Content>
-      </Layout>
+          </Layout.Content>
+        </Layout>
+      </BrowserRouter>
     </Provider>
   );
 };
